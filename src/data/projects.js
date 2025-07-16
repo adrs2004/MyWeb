0689526deafad6f75c1489eba61288ba9041f246
Adarsh Kumar Pathak <122677349+adrs2004@users.noreply.github.com>
2025-07-16 20:12:46 +0530

    Updated Project.js

---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+export const projects = [
+    {
+    id: 1,
+    title: "Hindalco-Nexux",
+    subtitle: "Internal Issue Tracking and Resolution System",
+    image: "/projects/7.png",
+    link: "https://github.com/adrs2004/Hindalco-Nexus"
+  },
+  {
+    id: 2,
+    title: "RealEstate Platform",
+    subtitle: "Full-featured property listing with secure authentication and payments",
+    image: "/projects/1.png",
+    link: "https://github.com/adrs2004/RealEstate"
+  },
+  {
+    id: 3,
+    title: "Hospital Management System",
+    subtitle: "Streamlined patient and doctor management with appointment booking",
+    image: "/projects/2.png",
+    link: "https://github.com/adrs2004/Medical-CareHub"
+  },
+  {
+    id: 4,
+    title: "Marvel",
+    subtitle: "Explore all Marvel characters, powers, Infinity Stones, movies, comics, and watch their signature moves in short clips.",
+    image: "/projects/3.png",
+    link: "https://github.com/adrs2004/Marvel"
+  },
+  {
+    id: 5,
+    title: "My Portfolio",
+    subtitle: "Personal portfolio showcasing skills, projects, CV, and contact options",
+    image: "/projects/4.png",
+    link: "https://github.com/adrs2004/My-Portfolio"
+  },
+  {
+    id: 6,
+    title: "TaskPilot",
+    subtitle: "Task management app with user auth, real-time clock, and MySQL backend",
+    image: "/projects/5.png",
+    link: "https://github.com/adrs2004/TaskPilot"
+  },
+  {
+    id: 7,
+    title: "Animated Web",
+    subtitle: "Creative UI animations with smooth transitions and effects",
+    image: "/projects/6.png",
+    link: "https://github.com/adrs2004/Scvanger-Hunt-Event"
+  }
+];
